e18a3ca3a8a495d264aaf2ffdeef5db6a9d3d651
chp <charprat@yahoo.fr>
2022-10-14 12:54:01 +0200

    add fit of nb.tides

--- libalgae.py
+++ libalgae.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 
 def load_algae():
@@ -34,7 +34,7 @@ class AlgaeClassifier:
     attributes:
         distrib_dict (dict):
             keys: name of the features of the dataset
-            values: tuple of 2 distribition to fit, 1st for label=0
+            values: tuple of 2 distributions to fit, 1st for label=0
               and the 2nd for label=1
         log_prior (float): log(p(y=0)/p(y=1))
 
@@ -50,8 +50,9 @@ class AlgaeClassifier:
             self.log_prior = np.log((1-p1)/p1)
         for name, dists in self.distrib_dict.items():
             for label, dist in enumerate(dists):
-                dist.params = dist.dist.fit(
-                    X_train.loc[y_train == label, name])
+                if dist.params is None:
+                    dist.params = dist.dist.fit(
+                        X_train.loc[y_train == label, name])
 
     def plot_fitted_distrib(self, ax, name, label):
         distp = self.distrib_dict[name][label]
@@ -63,14 +64,22 @@ class AlgaeClassifier:
         llh = [np.zeros(X.shape[0]), np.zeros(X.shape[0])]
         for name, distps in self.distrib_dict.items():
             for label, distp in enumerate(distps):
-                llh[label] = llh[label] + np.log(
-                    distp.dist.pdf(X[name], *distp.params)+1e-20)
+                if hasattr(distp.dist, 'pdf'):
+                    llh[label] = llh[label] + np.log(
+                        distp.dist.pdf(X[name], *distp.params)+1e-20)
+                else: # discrete ditribution
+                    llh[label] = llh[label] + np.log(
+                        distp.dist.pmf(X[name], *distp.params)+1e-20)
+
         return llh[1] > llh[0] + self.log_prior
 
     def get_scores(self, X, y_true):
         y_pred = self.predict(X)
-        return (accuracy_score(y_true, y_pred),
-                f1_score(y_true, y_pred))
+        return {"accuracy": accuracy_score(y_true, y_pred),
+                "f1": f1_score(y_true, y_pred),
+                "precision": precision_score(y_true, y_pred),
+                "recall": recall_score(y_true, y_pred),
+                }
 
 
 if __name__ == "__main__":
@@ -87,6 +96,7 @@ if __name__ == "__main__":
         "wind": (Distrib(stats.norm), Distrib(stats.norm)),
         #"temperature": (Distrib(stats.norm), Distrib(stats.norm)),
         "discharge": (Distrib(stats.norm), Distrib(stats.weibull_min)),
+        "nb.tides": (Distrib(stats.poisson, [5]), Distrib(stats.poisson, [6.5])),
     }
 
     clf = AlgaeClassifier(distrib_dict)
@@ -96,8 +106,7 @@ if __name__ == "__main__":
         data=[clf.get_scores(X_train, y_train),
               clf.get_scores(X_test, y_test)],
         index=["train", "test"],
-        columns=["accuracy", "f1"]
     )
 
-    with pd.option_context('display.float_format', '{:0.2f}'.format):
+    with pd.option_context('display.float_format', '{:0.3f}'.format):
         print(results)
